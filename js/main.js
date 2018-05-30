b824913b0beb9bc996eb35898d05da5aff4d6063
palacz.dominik@gmail.com <palacz.dominik@gmail.com>
2018-05-30 13:49:28 +0200

    Zadanie 2

--- js/main.js
+++ js/main.js
@@ -7,35 +7,55 @@ document.addEventListener("DOMContentLoaded", function () {
     //for (var i = 0; i < menuEl.length; i++) {
     //    console.log(menuEl[i]);
     //    console.log(i);
+    /*
+        menuEl[2].addEventListener('click', function () {
+            console.warn('mousover');
 
-    menuEl[2].addEventListener('click', function () {
-        console.warn('mousover');
+            var navUl = document.createElement('ul');
+            navUl.classList.add('about');
+            var navLiA = document.createElement('li');
+            var navLiN = document.createElement('li');
+            var navLiH = document.createElement('li');
+            navLiA.innerHTML = '<a href="#">Aktualności</a>';
+            navLiN.innerHTML = '<a href="#">Nasz team</a>';
+            navLiH.innerHTML = '<a href="#">Historia</a>';
 
-        var navUl = document.createElement('ul');
-        navUl.classList.add('about');
-        var navLiA = document.createElement('li');
-        var navLiN = document.createElement('li');
-        var navLiH = document.createElement('li');
-        navLiA.innerHTML = '<a href="#">Aktualności</a>';
-        navLiN.innerHTML = '<a href="#">Nasz team</a>';
-        navLiH.innerHTML = '<a href="#">Historia</a>';
+            navUl.appendChild(navLiA);
+            navUl.appendChild(navLiN);
+            navUl.appendChild(navLiH);
 
-        navUl.appendChild(navLiA);
-        navUl.appendChild(navLiN);
-        navUl.appendChild(navLiH);
+            console.log(navUl);
 
-        console.log(navUl);
+            menuEl[2].appendChild(navUl);
 
-        menuEl[2].appendChild(navUl);
 
-       // menuEl[2].addEventListener('mouseout', function () {
-        //    menuEl[2].removeChild(navUl)
-        //});
+        });
 
+    */
+
+    var hid = document.querySelectorAll('.text-in-left');
+    console.log(hid);
+
+    var center = document.querySelector('.center');
+    center.addEventListener('mouseover', function () {
+        hid[1].style.display = 'none';
+    });
+
+    var center = document.querySelector('.center');
+    center.addEventListener('mouseout', function () {
+        hid[1].style.display = 'block';
     });
 
 
+    var left = document.querySelector('.left');
+    left.addEventListener('mouseover', function () {
+        hid[0].style.display = 'none';
+    });
 
+    var left = document.querySelector('.left');
+    left.addEventListener('mouseout', function () {
+        hid[0].style.display = 'block';
+    });
 
 
 });
