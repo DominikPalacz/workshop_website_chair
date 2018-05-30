e7d0971c84fa3fc53dd6c0842718fcb6f239424a
palacz.dominik@gmail.com <palacz.dominik@gmail.com>
2018-05-30 12:37:01 +0200

    create click

--- js/main.js
+++ js/main.js
@@ -4,38 +4,38 @@ document.addEventListener("DOMContentLoaded", function () {
     var menuEl = document.getElementsByClassName('header-menu');
     console.warn(menuEl);
 
-    for (var i = 0; i < menuEl.length; i++) {
-        console.log(menuEl[i]);
-        console.log(i);
+    //for (var i = 0; i < menuEl.length; i++) {
+    //    console.log(menuEl[i]);
+    //    console.log(i);
 
-        menuEl[i].addEventListener('mouseover', function () {
-            console.warn('mousover');
+    menuEl[2].addEventListener('click', function () {
+        console.warn('mousover');
+
+        var navUl = document.createElement('ul');
+        navUl.classList.add('about');
+        var navLiA = document.createElement('li');
+        var navLiN = document.createElement('li');
+        var navLiH = document.createElement('li');
+        navLiA.innerHTML = '<a href="#">Aktualności</a>';
+        navLiN.innerHTML = '<a href="#">Nasz team</a>';
+        navLiH.innerHTML = '<a href="#">Historia</a>';
+
+        navUl.appendChild(navLiA);
+        navUl.appendChild(navLiN);
+        navUl.appendChild(navLiH);
+
+        console.log(navUl);
+
+        menuEl[2].appendChild(navUl);
+
+       // menuEl[2].addEventListener('mouseout', function () {
+        //    menuEl[2].removeChild(navUl)
+        //});
+
+    });
 
 
-            var navUl = document.createElement('ul');
 
-            var navLiA = document.createElement('li');
-            var navLiN = document.createElement('li');
-            var navLiH = document.createElement('li');
-
-            navLiA.innerHTML = '<a href="#">Aktualności</a>';
-            navLiN.innerHTML = '<a href="#">Nasz team</a>';
-            navLiH.innerHTML = '<a href="#">Historia</a>';
-
-            navUl.appendChild(navLiA);
-            navUl.appendChild(navLiN);
-            navUl.appendChild(navLiH);
-
-            console.log(navUl);
-
-            
-
-            var oFirmie = document.getElementById()
-
-        });
-
-
-    }
 
 
 });
